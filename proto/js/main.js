d0c9e4e03a046fef975f2ebf20187f9a147e3f32
mark <mark@redballoondesign.co.uk>
2016-07-13 10:32:04 +0100

    Added Slim

--- proto/js/main.js
+++ proto/js/main.js
@@ -5,8 +5,68 @@ function initMap() {
         zoom: 8
     });
 }
-
-
-function openNav(){
+function toggleNav(){
     console.log('toggle nav');
+}
+
+
+function serverComunication( request ){
+    //  Uses AJAX to communicate with the server
+    $.ajax({
+        url: '/path/to/file',
+        type: 'default GET (Other values: POST)',
+        dataType: 'default: Intelligent Guess (Other values: xml, json, script, or html)',
+        data: {param1: 'value1'}
+    })
+    .done(function(data) {
+        console.log("success");
+        // Decode data?
+        return data;
+    })
+    .fail(function() {
+        console.log("error");
+    })
+    .always(function() {
+        console.log("complete");
+    });
+};
+
+
+// Get the users position:
+function getLocation(){
+
+    var userPos = {lat:51, lng:1};
+
+    if (navigator.geolocation) {
+        // uses user location from device.
+        navigator.geolocation.getCurrentPosition(function(position){
+            userPos['lat'] = position.coords.latitude;
+            userPos['lng'] = position.coords.longitude;
+        }, function(){
+            var fallbackoutput = locationFallback();
+            userPos['lat'] = fallbackoutput.lat;
+            userPos['lng'] = fallbackoutput.lng;
+        });
+    } else {
+        var fallbackoutput = locationFallback();
+        userPos['lat'] = fallbackoutput.lat;
+        userPos['lng'] = fallbackoutput.lng;
+    };
+
+    return userPos;
+
+}
+
+function locationFallback(){
+
+    var userPosIP;
+
+    $.getJSON('http://ipinfo.io', function(data){
+        var dataArray = data.loc.split(',');
+        userPosIP = {
+            lat: parseInt(dataArray[0]),
+            lng: parseInt(dataArray[1])
+        };
+        console.log(dataArray);
+    });
 }
---
+++ src/public/index.php
@@ -0,0 +1,15 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+require_once 'config.php';
+
+$app = new \Slim\App(["settings" => $config]);
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+$app->run();
